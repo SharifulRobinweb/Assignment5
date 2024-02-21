514df4060ca4186692e9d5d0b047fe24a932cf60
SharifulRobinweb <shariful157968@gmail.com>
2024-02-22 01:41:32 +0600

    grand total

--- scripts/script.js
+++ scripts/script.js
@@ -6,63 +6,99 @@ let remain = 16;
 
 for (const btn of allBtn) {
     btn.addEventListener('click', function (e) {
+
         count = count + 1;
         remain = remain - 1;
+        // setAttribute("disabled",true);
 
-        if( count <= 4){
+
+        if (count <= 4) {
             const selectedSeat = document.getElementById('selected-seat');
-        selectedSeat.innerText = count;
+            selectedSeat.innerText = count;
+            const selectedSeats = document.getElementById('selected-seat-left');
+            selectedSeats.innerText = count;
 
-        btn.classList.add('bg-green-400');
-        btn.classList.add('text-white');
-        
-
-
-        const remainSeat = document.getElementById('remain-seat');
-        remainSeat.innerText = remain;
-        // inside selected seat
+            btn.classList.add('bg-green-400');
+            btn.classList.add('text-white');
 
 
 
-        const seat = e.target.parentNode.childNodes[1].innerText;
-        // console.log(e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[9].childNodes[3].childNodes[1].childNodes[5].childNodes[3].childNodes[1].childNodes[1].innerText);
-        const price = e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[9].childNodes[3].childNodes[1].childNodes[5].childNodes[3].childNodes[1].childNodes[1].innerText;
 
-        // const economoy = Economy;
 
-        const selectedContainer = document.getElementById("selected-place-container");
+            const remainSeat = document.getElementById('remain-seat');
+            remainSeat.innerText = remain;
+            // inside selected seat
 
-        const li = document.createElement("li");
-        li.classList.add('flex');
-        li.classList.add('gap-x-20');
-        const p = document.createElement("p");
-        p.innerText = seat;
-        const p2 = document.createElement("p");
-        p2.innerText = "economoy";
-        const p3 = document.createElement("p");
-        p3.innerText = price;
 
-        li.appendChild(p);
-        li.appendChild(p2);
-        li.appendChild(p3);
-        selectedContainer.appendChild(li);
 
-        // Total cost
-        const totalCost = document.getElementById("total-cost").innerText;
-        const convertedTotalCost = parseInt(totalCost);
 
-        document.getElementById('total-cost').innerText = convertedTotalCost + parseInt(price);
+            const seat = e.target.parentNode.childNodes[1].innerText;
+            // console.log(e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[9].childNodes[3].childNodes[1].childNodes[5].childNodes[3].childNodes[1].childNodes[1].innerText);
+            const price = e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[9].childNodes[3].childNodes[1].childNodes[5].childNodes[3].childNodes[1].childNodes[1].innerText;
+
+            // const economoy = Economy;
+
+
+            const selectedContainer = document.getElementById("selected-place-container");
+
+            const li = document.createElement("li");
+            li.classList.add('flex');
+            li.classList.add('gap-x-20');
+            const p = document.createElement("p");
+            p.innerText = seat;
+            const p2 = document.createElement("p");
+            p2.innerText = "economoy";
+            const p3 = document.createElement("p");
+            p3.innerText = price;
+
+
+            li.appendChild(p);
+            li.appendChild(p2);
+            li.appendChild(p3);
+            selectedContainer.appendChild(li);
+
+
+            // Total cost
+            const totalCost = document.getElementById("total-cost").innerText;
+            const convertedTotalCost = parseInt(totalCost);
+            const sum = convertedTotalCost + parseInt(price); 
+
+            document.getElementById('total-cost').innerText = sum;
+            //Grand Total
+            const grandTotalText = document.getElementById("grand-cost").innerText;
+            const grandTotal = parseInt(grandTotalText);
+            const sum2 = grandTotal + parseInt(price);
+            document.getElementById("grand-cost").innerText = sum2;
+
+
+
+            e.target.setAttribute("disabled",true);
+
+
         }
-        else{
 
+        else {
+
+            alert("Already you selected 4 seat");
         }
 
 
-        
+
+
 
 
     })
 }
+
+
+
+
+
+
+
+
+
+
 
 function hideElementById(elementId) {
     const element = document.getElementById(elementId);
